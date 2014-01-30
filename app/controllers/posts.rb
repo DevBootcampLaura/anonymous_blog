5400b53fae9f4464ede146f41c0f72f2fda9ea8f
Laura <lleecrowe@gmail.com>
2014-01-30 12:13:01 -0600

    models and migrations up to date, create post works properly

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -2,6 +2,27 @@ get "/create_post" do
 	erb :create_post
 end
 
+post "/create_post" do
+  p = Post.create(title: params[:title], author: "anonymous", 
+               body: params[:body])
+
+  params[:category].split(",").each do |c|
+    c = c.strip
+    if Tag.find_by_category(c) == nil
+      t = Tag.create(category: c)
+      p.tags << t
+    else
+      p.tags << Tag.find_by_category(c)
+    end
+  end
+
+  redirect to "/"
+end
+
+
+
+
+
 get "/edit_post" do
   erb :edit_post
 end
